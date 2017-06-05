d94d677a4440f51b2de5564cf00fecc2ac661ab1
Lewis Akeroyd <lewis.akeroyd@su.plymouth.ac.uk>
2017-06-05 09:33:48 +0100

    removed temp fix for countdown 'done' alert

--- Scripts/functions.js
+++ Scripts/functions.js
@@ -193,14 +193,14 @@ $(document).ready(function () {
 
 //Call countdown plugin
 $(".countdown").countdown({
-    date: "3 june 2017 19:00:00", // add the countdown's end date (i.e. 3 november 2012 12:00:00)
+    date: "3 june 2018 19:00:00", // add the countdown's end date (i.e. 3 november 2012 12:00:00)
     format: "on" // on (03:07:52) | off (3:7:52) - two_digits set to ON maintains layout consistency
 },
 
 function () {
 
     // the code here will run when the countdown ends
-    alert("done!")
+    // alert("done!") // removed due to site wide alert display
 
 });
 
